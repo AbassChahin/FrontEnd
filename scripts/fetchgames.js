f49e3ebdf9984c69c475231f4e92191bba6cbe8f
Abass Chahin <Abass@Chahin.com>
2026-01-12 19:37:34 -0500

    Home Page Updated to show Error message on error. By default empty cards
    Search bar functionality implemented.
    
    Button filtering functionality implemented.
    
    Worked on CSS

--- scripts/fetchgames.js
+++ scripts/fetchgames.js
@@ -1,41 +1,46 @@
 
 async function getGames() {
     try {
-        const response = await fetch("http://localhost:8080/api/v1/game/allgames");
-
-        if (!response.ok) {
-            throw new Error("Bad Response " + response.status);
-        }
-
-        const games = await response.json();
         const gameGrid = document.querySelector(".game-grid");
 
         // Clear any potential cards already present
         gameGrid.innerHTML = "";
 
+
+        const response = await fetch("http://localhost:8080/api/v1/game/allgames");
+
+        if (!response.ok) {
+            const newCard = document.createElement("div");
+            newCard.classList.add("game-card");
+
+            newCard.innerHTML = `
+                    <div class="oops-error">
+                        <img src="images/error-logo.png" alt="">
+                        <h2>Oops! Something went wrong!</h2>
+                        <p>Please check again later!</p>
+                    </div>
+            `;
+            gameGrid.appendChild(newCard);
+            throw new Error("Bad Response " + response.status);
+        }
+
+        const games = await response.json();
         games.forEach(game => {
             const newCard = document.createElement("div");
             newCard.classList.add("game-card");
 
             newCard.innerHTML = `
                     <img src="http://localhost:8080/${game.imagePath}" onerror="this.src='images/Game.png'" alt="${game.title}">
-                    <div class="info-container">
+                    <div class="info-container" id="${game.platform}">
                         <h3>${game.title}</h3>
                         <p>$${game.salePrice}</p>
-                        <button>Get Game</button>
+                        <button onclick="window.open('${game.referenceURL}', '_blank');">Get Game</button>
                     </div>
             
             `;
 
             gameGrid.appendChild(newCard);
         });
-
-        for (let i = 0; i < games.length; i++) {
-            console.log(games[i]);
-        }
-
-
-
     } catch (error) {
         console.error("Error:", error);
     }
--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,70 @@
 
-// Show filter buttons as active when clicked
-const filterButtons = document.querySelectorAll(".filter-button");
+// Get buttons
+const epicGamesButton = document.querySelector("#epicgames");
+const steamGamesButton = document.querySelector("#steamgames");
 
-filterButtons.forEach(
-    button => {
-        button.addEventListener("click", function() {
-            button.classList.toggle("active");
-    });
+
+// Add event listeners and make buttons active when clicked
+epicGamesButton.addEventListener("click", () => {
+    const isActive = epicGamesButton.classList.toggle("active");
+
+    if (isActive) {
+        filterCardsByPlatform("#EPIC");
+    } else {
+        filterCardsByPlatform("All");
+    }
+})
+
+steamGamesButton.addEventListener("click", () => {
+    const isActive = steamGamesButton.classList.toggle("active");
+
+    if (isActive) {
+        filterCardsByPlatform("#STEAM");
+    } else {
+        filterCardsByPlatform("All");
+    }
+})
+
+function filterCardsByPlatform(platformId) {
+    const cards = document.querySelectorAll(".game-card");
+
+    if (platformId == "All") {
+        cards.forEach(card => {
+            card.style.display = "block";
+        })
+    } else {
+        cards.forEach(card => {
+            const match = card.querySelector(platformId);
+            if (match) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        })
+    }
+}
+
+// Get Search Bar
+const searchBar = document.querySelector(".searchbar");
+
+// Add event listener on input change
+searchBar.addEventListener("input", (e) => {
+    searchGames(e.target.value);
 });
+
+// Search Games
+function searchGames(input) {
+    const cards = document.querySelectorAll(".game-card");
+    const searchText = input.toLowerCase();
+
+    cards.forEach(card => {
+        const infoContainer = card.querySelector(".info-container");
+        const title = infoContainer.querySelector("h3")?.textContent.toLowerCase();
+
+        if (title && title.includes(searchText)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    })
+}
