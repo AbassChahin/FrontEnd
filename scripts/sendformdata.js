e964baa395e887c0895f5579d71235c208f2e355
Abass Chahin <Abass@Chahin.com>
2026-01-15 02:32:29 -0500

    Finalized formdata for contactus page to allow users to send messages through contact us form through API.
    Removed unneeded scripts in About and ContactUs page
    
    Fixed up styling and implemented success/error messaging for contactus form

---
+++ scripts/sendformdata.js
@@ -0,0 +1,55 @@
+const form = document.querySelector(".contact-form");
+form.addEventListener("submit", async function(event) {
+    event.preventDefault();
+    await sendMessage();
+});
+
+async function sendMessage() {
+    const submitButton = document.querySelector("#submit");
+    submitButton.disabled = true;
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const message = document.querySelector("#message").value;
+
+    console.log(name);
+    console.log(email);
+    console.log(message);
+
+    const requestBody = {
+        "name": name,
+        "email": email,
+        "message": message
+    }
+
+    try {
+        const response = await fetch("http://localhost:8080/api/v1/game/addmessage", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        if (!response.ok) {
+            throw new Error("Bad Response " + response.status);
+        }
+
+        const data = await response.json();
+        console.log("Server response:", data);
+
+        const successMessage = document.querySelector("#successMessage");
+        successMessage.style.display = "block";
+        form.reset();
+    } catch (error) {
+        const errorMessage = document.querySelector("#failureMessage");
+        errorMessage.style.display = "block";
+        console.error("Error sending message:", error);
+    } finally {
+        submitButton.disabled = false;
+    }
+}
+
+function successMessage() {
+
+}
